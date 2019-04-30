36c3b7d1dd5e016f06e8d7e39461502244155237
Sergio Alepuz <sergio@MacBook-Pro-de-Sergio.local>
2019-04-30 20:23:56 +0200

    feat(board-component): crea página de panel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
+import { BoardComponent } from './pages/board/board.component';
 
 const routes: Routes = [
+  { path: '', component: BoardComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '**', redirectTo: '' }
