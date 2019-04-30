462869895a54fa794b9f0b3d2b7c89968a0970ce
Sergio Alepuz <sergio@MacBook-Pro-de-Sergio.local>
2019-04-30 20:09:50 +0200

    feat(login-component): maqueta página de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  constructor() {}
+  formGroup: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {}
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
 }
