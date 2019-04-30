9420c39f4b0606ee14fa8890395d3d6bf1e4b4fc
Sergio Alepuz <sergio@MacBook-Pro-de-Sergio.local>
2019-04-30 20:12:00 +0200

    feat(login-component): muestra errores de validación

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,23 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  formGroup: FormGroup;
+    submitted: boolean;
+    formGroup: FormGroup;
 
-  constructor(
-    private formBuilder: FormBuilder,
-  ) {}
+    constructor(
+        private formBuilder: FormBuilder,
+    ) { }
 
-  ngOnInit(): void {
-    this.formGroup = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-  }
+    ngOnInit(): void {
+        this.formGroup = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
+
+    onSubmitted(): void {
+        this.submitted = true;
+    }
 
 }
