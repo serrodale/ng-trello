a9fd0179be790d0634e0e7a67a72b4acc6364ca7
Sergio Alepuz <sergio@MacBook-Pro-de-Sergio.local>
2019-04-30 17:43:03 +0200

    feat(register): maqueta la página de registro

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
+  formGroup: FormGroup;
+  loading: boolean;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {}
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmitted(): void {
+    console.log('Submitted');
+  }
 }
