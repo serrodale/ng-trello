38cf57e5d56c206a0f857a25e47ecc5eccee757c
Sergio Alepuz <sergio@MacBook-Pro-de-Sergio.local>
2019-05-01 13:24:48 +0200

    feat(board-component): añade input para crear listas

--- src/app/pages/board/board.component.ts
+++ src/app/pages/board/board.component.ts
@@ -17,6 +17,20 @@ export class BoardComponent {
     private authenticationService: AuthenticationService,
   ) {}
 
+  onKeydown(keyCode: number, input: HTMLInputElement): void {
+    const value: string = input.value;
+    
+    // Solo nos interesa la tecla enter y que el valor del input no sea una cadena vacía
+    if (keyCode !== 13 || !value.trim()) {
+      return;
+    }
+    
+    // TODO: crear lista
+
+    // Reseteamos el valor para que se borre el contenido que había antes
+    input.value = '';
+  }
+
   logout(): void {
     this.authenticationService.logout();
     this.router.navigate(['/login']);
