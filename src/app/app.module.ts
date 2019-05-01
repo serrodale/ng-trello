b316f0a2fc13ec59615670302a4bbbbf53e21808
Sergio Alepuz <sergio@MacBook-Pro-de-Sergio.local>
2019-05-01 13:32:58 +0200

    feat(jwt-interceptor): añade cabecera con ntoken a cualquier petición

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './pages/register/register.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoadingDirective } from './directives/loading.directive';
 import { AlertComponent } from './components/alert/alert.component';
 import { LoginComponent } from './pages/login/login.component';
 import { BoardComponent } from './pages/board/board.component';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,9 @@ import { BoardComponent } from './pages/board/board.component';
     HttpClientModule,
     ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
